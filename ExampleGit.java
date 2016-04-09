d5e3a0163c9fb9d175af62a32f5010f8a4484b4e
Ayyappareddy <Ayyappareddy@Ayyappareddy-PC>
2016-04-09 12:35:14 +0530

    initial check in

---
+++ ExampleGit.java
@@ -0,0 +1,20 @@
+
+public class ExampleGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean learning = true;
+		 
+	    if (learning) {
+	      System.out.println("Java programmer");
+	    }
+	    else {
+	      System.out.println("What are you doing here?");
+	    }
+
+	}
+
+}
